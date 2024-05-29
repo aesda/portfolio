cb4b6a85042edb6c01c5d6c53a343078c91cf703
SANGITA RAI <165255817+aesda@users.noreply.github.com>
2024-05-29 23:32:27 +1000

    added item

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+function hideOverlay() {
+    console.log('Closing Overlay')
+    document.querySelector('.transparent-overlay').classList.add('hide-overlay')
+
+}
+
+
+document.querySelector('.cross-btn').addEventListener('click', hideOverlay)
+setTimeout(()=> {
+    document.querySelector('.transparent-overlay').classList.add('remove')
+
+
+}, 1000 * 5)
